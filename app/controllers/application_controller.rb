299f80eafc21d0fa15147e45ead10fb9f848c3a9
jffaneco <jose.faneco@criticalsoftware.com>
2017-03-17 17:17:22 +0000

    Finish layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "a tua mãe aos saltos!"
+    render html: "....................!"
   end
 
 end
